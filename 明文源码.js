ce838dd9e55dfa477fc09c4a85ffbbea62315ffc
wz <wz7465@msn.com>
2025-01-27 14:50:18 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,13 +1,1 @@
-process.env['CHARUI_KAIGUAN'] = '1';             
-
-process.env['ERGOU_KAIGUAN'] = '1';              //如果填0    则不用通道直接走xray   此时duankou1填服务器端口，    duankou2乱填      2狗tok不填
-
-process.env['ERGOUYAOSHI'] = 'eyJhIjoiNmIwNGVkMmRkNzI5NGU1MjlhYWJlYTUwYzdjM2Y5MTgiLCJ0IjoiNjZiOTI3Y2MtNzI5Ny00NTRkLWFjYmEtZThkMTRkOGY4ZjcxIiwicyI6IllXWXpaR1F3TURrdFpHRmpPQzAwT0dKbExUbGhPRE10WVRJNFpqQXlPVFl4TmpJMyJ9';              
-
-process.env['LUJING'] = 'argo';           //path填:  /argo-mless >  vless > tro > ss  ...
-
-process.env['WOSHOUMIMA'] = '6210cd58-1f20-493f-8a32-1338cc9010c8';       //id
-
-process.env['DUANKOU'] = '8081';           //cf通道设置的端口  
-
-var DUANKOU2 = 25686;                      //服务对外的端口，就有填  伪装个动态主页  直接复制访问即可。。。platinum1.qloxhost.net:25686
+xxx
